a190771a46d310cd04fcbef34ab2115066110a42
Anton Angelov <aangelov@automatetheplanet.com>
2020-11-05 18:21:10 +0200

    add examples pages with base page

---
+++ Chapter 4- Tests Readability/src/main/java/pages/v2/singlefilepageobjectbasepage/CartPage.java
@@ -0,0 +1,72 @@
+package pages.v2.singlefilepageobjectbasepage;
+
+import core.Driver;
+import core.Element;
+import org.openqa.selenium.By;
+
+public class CartPage extends BaseEShopPage {
+    public CartPage(Driver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected String getUrl() {
+        return "http://demos.bellatrix.solutions/cart/";
+    }
+
+    private Element couponCodeTextField() {
+        return driver.findElement(By.id("coupon_code"));
+    }
+
+    private Element applyCouponButton() {
+        return driver.findElement(By.cssSelector("[value*='Apply coupon']"));
+    }
+
+    private Element quantityBox() {
+        return driver.findElement(By.cssSelector("[class*='input-text qty text']"));
+    }
+
+    private Element updateCart() {
+        return driver.findElement(By.cssSelector("[value*='Update cart']"));
+    }
+
+    private Element messageAlert() {
+        return driver.findElement(By.cssSelector("[class*='woocommerce-message']"));
+    }
+
+    private Element totalSpan() {
+        return driver.findElement(By.xpath("//*[@class='order-total']//span"));
+    }
+
+    private Element proceedToCheckout() {
+        return driver.findElement(By.cssSelector("[class*='checkout-button button alt wc-forward']"));
+    }
+
+    public void applyCoupon(String coupon) throws InterruptedException {
+        couponCodeTextField().typeText(coupon);
+        applyCouponButton().click();
+        driver.waitForAjax();
+    }
+
+    public void increaseProductQuantity(int newQuantity) throws InterruptedException {
+        quantityBox().typeText(String.valueOf(newQuantity));
+        updateCart().click();
+        driver.waitForAjax();
+    }
+
+    public void clickProceedToCheckout()
+    {
+        proceedToCheckout().click();
+        driver.waitUntilPageLoadsCompletely();
+    }
+
+    public String getTotal()
+    {
+        return totalSpan().getText();
+    }
+
+    public String getMessageNotification()
+    {
+        return messageAlert().getText();
+    }
+}
