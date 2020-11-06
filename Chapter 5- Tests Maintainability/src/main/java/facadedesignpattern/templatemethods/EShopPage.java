d40e3ab397cd69b92d44583799267b2b3e509562
Anton Angelov <aangelov@automatetheplanet.com>
2020-11-06 16:24:34 +0200

    add examples for template methods facades

---
+++ Chapter 5- Tests Maintainability/src/main/java/facadedesignpattern/templatemethods/EShopPage.java
@@ -0,0 +1,26 @@
+package facadedesignpattern.templatemethods;
+
+import core.Driver;
+import pages.v3.singlefilepageobjectssections.Sections.CartInfoSection;
+import pages.v3.singlefilepageobjectssections.Sections.MainMenuSection;
+import pages.v3.singlefilepageobjectssections.Sections.SearchSection;
+
+public abstract class EShopPage {
+    protected final Driver driver;
+
+    public EShopPage(Driver driver) {
+        this.driver = driver;
+    }
+
+    public MainMenuSection mainMenuSection() {
+        return new MainMenuSection(driver);
+    }
+
+    public CartInfoSection cartInfoSection() {
+        return new CartInfoSection(driver);
+    }
+
+    public SearchSection searchSection() {
+        return new SearchSection(driver);
+    }
+}
