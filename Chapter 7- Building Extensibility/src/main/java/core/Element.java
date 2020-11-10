2646a3346cd5bb54f8fffeb60db7e51edef9d9ca
Anton Angelov <aangelov@automatetheplanet.com>
2020-11-10 12:32:53 +0200

    add locators for element

--- Chapter 7- Building Extensibility/src/main/java/core/Element.java
+++ Chapter 7- Building Extensibility/src/main/java/core/Element.java
@@ -1,6 +1,9 @@
 package core;
 
+import core.locators.FindStrategy;
 import org.openqa.selenium.By;
+
+import java.util.List;
 
 public abstract class Element {
     public abstract By getBy();
@@ -10,5 +13,20 @@ public abstract class Element {
     public abstract void typeText(String text) throws InterruptedException;
     public abstract void click();
     public abstract String getAttribute(String attributeName);
-    public abstract Element findElement(By locator);
+    public abstract void waitToExists();
+    public abstract Element findById(String id);
+    public abstract Element findByXPath(String xpath);
+    public abstract Element findByTag(String tag);
+    public abstract Element findByClass(String cssClass);
+    public abstract Element findByCss(String css);
+    public abstract Element findByLinkText(String linkText);
+    public abstract List<Element> findAllById(String id);
+    public abstract List<Element> findAllByXPath(String xpath);
+    public abstract List<Element> findAllByTag(String tag);
+    public abstract List<Element> findAllByClass(String cssClass);
+    public abstract List<Element> findAllByCss(String css);
+    public abstract List<Element> findAllByLinkText(String linkText);
+
+    public abstract List<Element> findAll(FindStrategy findStrategy);
+    public abstract Element find(FindStrategy findStrategy);
 }
