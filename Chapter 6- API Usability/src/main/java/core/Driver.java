dcae10ff75ace542227f45a5bbbce24f62ac68ff
Anton Angelov <aangelov@automatetheplanet.com>
2021-01-12 16:50:20 +0200

    refactoring chapter 6

--- Chapter 6- API Usability/src/main/java/core/Driver.java
+++ Chapter 6- API Usability/src/main/java/core/Driver.java
@@ -13,24 +13,5 @@
 
 package core;
 
-import org.openqa.selenium.By;
-
-import java.util.List;
-
 public abstract class Driver implements NavigationService, BrowserService, CookiesService, ElementFindService, DialogService {
-    public abstract void start(Browser browser);
-
-    @Override
-    public abstract void quit();
-
-    public abstract void goToUrl(String url);
-
-    public abstract Element findElement(By locator);
-
-    public abstract List<Element> findElements(By locator);
-
-    public abstract void waitForAjax();
-
-    @Override
-    public abstract void waitUntilPageLoadsCompletely();
 }
